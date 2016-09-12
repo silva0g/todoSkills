5dd5a7c9d2188ad19c9d9f5bd417730a2610cd38
silva0g <silvanazinha@gmail.com>
2016-09-12 23:46:49 +0200

    Added the about page and the routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
   def home
   end
+
+  # here is the about action 
+  def about 
+  	
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # Here we're gonna add the routes for our page home
   root 'pages#home'
+  get 'about' => "pages#about"
 end
 
