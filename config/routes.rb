f4b1b07f1ca4eb9df0ec4f219337504c8f9d097c
silva0g <silvanazinha@gmail.com>
2016-09-12 23:25:23 +0200

    Created the Page Home and added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Here we're gonna add the routes for our page home
+  root 'pages#home'
 end
+
